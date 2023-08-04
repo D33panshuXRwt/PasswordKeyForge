b6770a4d8980484ca9ecd1270f699458a47eb0a5
DRwt00 <deepanshurawat298@gmail.com>
2023-08-04 17:19:22 +0530

    Initial commit

--- passwordkeyforge.py
+++ passwordkeyforge.py
@@ -1,12 +1,25 @@
 import itertools
 import string
 
-characters = string.ascii_letters
-length = int(input("Enter number of characters needed in password: "))
+print("<<----- Enter the type of password want to create ----->>")
+print("""
+    1. Default (contains only charcters both UPPER-CASE and lower-case)
+    2. Lower-Case only
+    3. 
+    
+    2. Numbers (Use for OTP/PIN)
+    3. ALL-IN (contains everthing Characters/Numbers,Special characters)
+""")
 
+
+
+
+
+characters = string.ascii_letters
 
 try:
+    length = int(input("Enter number of characters needed in password: "))
     for combination in itertools.combinations(characters, length):
         print(combination)
-except:
+except ValueError:
     print("<<---- Enter a numerical value to execute file ---->>")
