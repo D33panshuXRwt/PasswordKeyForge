479363303c4840a54f9328d1ecda8c76bd4d1723
Deepanshu <deepanshurawat298@gmail.com>
2023-08-07 22:46:55 +0530

    iUsing function and recussion to to remove error

--- passwordkeyforge.py
+++ passwordkeyforge.py
@@ -2,17 +2,23 @@ import itertools
 import string
 
 
-print("""
+def print_clear_ascii_art():
+    clear_ascii_art = """
 _____                                    _ _               __                     
-|  __ \                                  | | |            / _|                    
+|  __ \\                                  | | |            / _|                    
 | |__) |_ _ ___ _____      _____  _ __ __| | | _____ _   _| |_ ___  _ __ __ _  ___ 
-|  ___/ _` / __/ __\ \ /\ / / _ \| '__/ _` | |/ / _ \ | | |  _/ _ \| '__/ _` |/ _ 
-| |  | (_| \__ \__ \\ V  V / (_) | | | (_| | | <  __/ |_| | || (_) | | | (_| |  __/
-|_|   \__,_|___/___/ \_/\_/ \___/|_|  \__,_|_|\_\___|\__, |_| \___/|_|  \__, |\___|
+|  ___/ _` / __/ __\\ \\ /\\ / / _ \\| '__/ _` | |/ / _ \\ | | |  _/ _ \\| '__/ _` |/ _ \\
+| |  | (_| \\__ \\__ \\\\ V  V / (_) | | | (_| | | <  __/ |_| | || (_) | | | (_| |  __/
+|_|   \\__,_|___/___/ \\_/\\_/ \\___/|_|  \\__,_|_|\\_\\___|\\__, |_| \\___/|_|  \\__, |\\___|
                                                        __/ |              __/ |     
                                                       |___/              |___/      
+"""
 
-""")
+    print(clear_ascii_art)
+
+# Call the function to print the ASCII art for "clear"
+print_clear_ascii_art()
+
 
 print("<<----- Types of Password Method ----->>")
 print("""
@@ -22,23 +28,35 @@ print("""
     4. Numbers (Use for OTP/PIN)
     5. ALL-IN (contains everthing Characters/Numbers,Special characters)
 """)
-askpswd = input("Enter your choice: ")
 
-if askpswd == "1" or "Default" or int(askpswd) == 1:
-    characters = string.ascii_letters
-elif askpswd == "2" or "Lower-Case" or int(askpswd) ==2:
-    characters = string.ascii_lowercase
-elif askpswd == "3" or "Upper-Case" or int(askpswd) ==3:
-    characters = string.ascii_uppercase
-elif askpswd == "4" or "Numbers" or int(askpswd) ==4:
-    characters = string.digits
-elif askpswd =="5" or "ALL-IN" or int(askpswd) == 5:
-    characters = string.printable
 
+def ask_user():
+
+    askpswd = input("Enter your choice: ")
+
+    if askpswd == "1" or "Default" or int(askpswd) == 1:
+        characters = string.ascii_letters
+    elif askpswd == "2" or "Lower-Case" or int(askpswd) ==2:
+        characters = string.ascii_lowercase
+    elif askpswd == "3" or "Upper-Case" or int(askpswd) ==3:
+        characters = string.ascii_uppercase
+    elif askpswd == "4" or "Numbers" or int(askpswd) ==4:
+        characters = string.digits
+    elif askpswd =="5" or "ALL-IN" or int(askpswd) == 5:
+        characters = string.printable
+    else:
+        print("Choose Valid Option")
+        ask_user()
+
+
+ask_user()
 
 try:
+    i = 0
     length = int(input("Enter number of characters needed in password: "))
     for combination in itertools.combinations(characters, length):
         print(combination)
+        i=i+1
+    print(i)
 except ValueError:
     print("<<---- Enter a numerical value to execute file ---->>")
