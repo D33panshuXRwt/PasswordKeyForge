2a541651ecd7ae7f28544c5fc5ddb6c346581f18
Deepanshu <deepanshurawat298@gmail.com>
2023-08-13 15:47:53 +0530

    commited

--- passwordkeyforge.py
+++ passwordkeyforge.py
@@ -3,19 +3,19 @@ import string
 
 def print_clear_ascii_art():
     clear_ascii_art = """
-_____                                    _ _               __                     
-|  __ \\                                  | | |            / _|                    
-| |__) |_ _ ___ _____      _____  _ __ __| | | _____ _   _| |_ ___  _ __ __ _  ___ 
-|  ___/ _` / __/ __\\ \\ /\\ / / _ \\| '__/ _` | |/ / _ \\ | | |  _/ _ \\| '__/ _` |/ _ \\
-| |  | (_| \\__ \\__ \\\\ V  V / (_) | | | (_| | | <  __/ |_| | || (_) | | | (_| |  __/
-|_|   \\__,_|___/___/ \\_/\\_/ \\___/|_|  \\__,_|_|\\_\\___|\\__, |_| \\___/|_|  \\__, |\\___|
+ ______                                    ___              ___
+ |  __ \\                                  | | |            / __|                    
+ | |__) |_ _ ___ _____      _____  _ __ __| | | _____ _   _| |_ ___  _ __ __ _  ___ 
+ |  ___/ _` / __/ __\\ \\ /\\ / / _ \\| '__/ _` | |/ / _ \\ | | |  _/ _ \\| '__/ _` |/ _ \\
+ | |  | (_| \\__ \\__ \\\\ V  V / (_) | | | (_| | | <  __/ |_| | || (_) | | | (_| |  __/
+ |_|   \\__,_|___/___/ \\_/\\_/ \\___/|_|  \\__,_|_|\\_\\___|\\__, |_| \\___/|_|  \\__, |\\___|
                                                        __/ |              __/ |     
                                                       |___/              |___/      
-"""
+    """
     print(clear_ascii_art)
 
-
 print_clear_ascii_art()
+    
 
 print("<<----- Types of Password Methods ----->>")
 print("""
@@ -24,6 +24,7 @@ print("""
     3. Upper-Case only 
     4. Numbers (Use for OTP/PIN)
     5. ALL-IN (contains all Characters/Numbers, Special characters)
+    6. Custom
 """)
 
 def get_character_set(choice):
@@ -38,17 +39,18 @@ def get_character_set(choice):
     elif choice == 5:
         return string.printable
     elif choice == 6:
-        characters = input("Enter yoyr favourable characters: ")
+        characters = input("Enter your favorable characters: ")
+        return characters
     else:
         print("Choose a Valid Option")
         return None
 
 def generate_passwords(characters, length):
-    i = 0
+    passwords = []
     for combination in itertools.product(characters, repeat=length):
-        print(''.join(combination))
-        i += 1
-    return i
+        passwd = ''.join(combination)
+        passwords.append(passwd)
+    return passwords
 
 try:
     askpswd = int(input("Enter your choice: "))
@@ -60,9 +62,18 @@ try:
         characters = get_character_set(askpswd)
         
     length = int(input("Enter the number of characters needed in the password: "))
-    count = generate_passwords(characters, length)
-    print("Total passwords generated:", count)
-
-except:
-    print("<<---- Enter a numerical value to execute the file ---->>")
+    passwords = generate_passwords(characters, length)
     
+    filename = input("Enter file name to save passwords: ")
+    with open(filename, "w") as f:
+        for passwd in passwords:
+            f.write(passwd + "\n")
+    
+    print("Total passwords generated:", len(passwords))
+    print("Passwords saved in", filename)
+
+except ValueError:
+    print("<<---- Enter a numerical value to execute the file ---->>")
+except Exception as e:
+    print("An error occurred:", e)
+ 
